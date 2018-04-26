84bec8f4a9348c9d773c6586ad335995685d723f
young <onlysuncolour@sina.com>
2018-04-26 16:46:22 +0800

    new: index & package.json

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-return 0;
+;(function (name, definition) {
+  var hasDefine = typeof define === 'function',
+    hasExports = typeof module !== 'undefined' && module.exports;
+  if (hasDefine) {
+    define(definition);
+  } else if (hasExports) {
+    module.exports = definition();
+  } else {
+    this[name] = definition();
+  }
+})('ymint', function () {
+  return 0
+});
